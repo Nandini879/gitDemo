65531ab3e9d9b389b1e7fb5040dffa7163355424
nandini <hiremath.nandini2@gmail.com>
2019-11-20 17:07:50 +0530

    committedat 4 pm

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -6,6 +6,8 @@ public class RestAPITest {
 	@Test
 	public void Postjira() {
 		System.out.println("jira");
+		System.out.println("jira1");
+		System.out.println("jira2");
 	}
 	@Test
 	public void deleteRest() {
