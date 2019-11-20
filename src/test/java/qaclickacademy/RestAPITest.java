bed14a0c7011ad62862cb6573d96355672873f56
nandini <hiremath.nandini2@gmail.com>
2019-11-20 16:25:52 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void Postjira() {
+		System.out.println("jira");
+	}
+	@Test
+	public void deleteRest() {
+		System.out.println("deletea");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	@Test
+	public void seleniumbaseauto() {
+		System.out.println("Hi Maven project wih sele");
+	}
+	@Test
+	public void elementUI() {
+		System.out.println("Hi elements");
+	}
+}
